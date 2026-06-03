59ecc24deb8e4fc12a7c050f58331e9a3053ca28
Igor Ventura <igorventura1705@gmail.com>
2026-06-02 22:30:06 -0300

    arquitetura inicial do projeto

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.send("Projeto funcionando!");
+});
+
+app.listen(3000, () => {
+    console.log("Servidor rodando...");
+});
